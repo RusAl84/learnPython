6be82cf264d0baea0fc33433e6ea1a13b59113e6
Aleksey <rusal@bk.ru>
2022-10-13 18:42:21 +0300

    file csv write

---
+++ ex_file_csv.py
@@ -0,0 +1,16 @@
+import csv
+import random
+
+users = []
+for i in range(10):
+    line = []
+    line.append(f"Ёжик {i}")
+    import random
+    line.append(f"Возраст {random.randint(1, 5)}")
+    users.append(line)
+
+# print(users)
+
+with open("data.csv", "w", newline="", encoding="UTF8") as file:
+    writer = csv.writer(file)
+    writer.writerows(users)
