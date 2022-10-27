5d209bcc441f45ed91c31ec48115f2a7f452760f
Aleksey <rusal@bk.ru>
2022-10-27 18:59:12 +0300

    add messanger server

---
+++ app_messanger.py
@@ -0,0 +1,45 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+ListOfMessages = []
+
+@app.route('/')
+def dafault_route():
+    return 'Messenger Flask server is running! ' \
+        '<br> <a href="/status">Check status</a>'
+
+@app.route('/status')
+def status():
+    return {
+        'messages_count': len(ListOfMessages)
+    }
+
+# отправка сообщений
+@app.route("/api/Messanger", methods=['POST'])
+def SendMessage():
+    msg = request.json
+    print(msg)
+    # messages.append({"UserName":"RusAl","MessageText":"Privet na sto let!!!","TimeStamp":"2021-03-05T18:23:10.932973Z"})
+    ListOfMessages.append(msg)
+    print(msg)
+    msgtext = f"{msg['UserName']} <{msg['TimeStamp']}>: {msg['MessageText']}"
+    print(f"Всего сообщений: {len(ListOfMessages)} Посланное сообщение: {msgtext}")
+    return f"Сообщение отослано успшно. Всего сообщений: {len(ListOfMessages)} ", 200
+
+
+# получение сообщений
+@app.route("/api/Messanger/<int:id>")
+def GetMessage(id):
+    print(id)
+    if id >= 0 and id < len(ListOfMessages):
+        print(ListOfMessages[id])
+        return ListOfMessages[id], 200
+    else:
+        return "Not found", 400
+
+
+if __name__ == '__main__':
+    app.run()
