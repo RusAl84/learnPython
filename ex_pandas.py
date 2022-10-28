61eae307142cbbc2d3b1eb712a4fce8e9b566461
Aleksey <rusal@bk.ru>
2022-10-28 20:02:11 +0300

    fix

--- ex_pandas.py
+++ ex_pandas.py
@@ -4,12 +4,13 @@ df = pd.read_excel("1.xlsx")
 l = df.values.tolist()
 
 print(df)
-print(l[0][0])
+# print(l[0][0])
 avr = 0
 for item in l:
     avr += int(item[1])
 avr /= len(l)
 avr = int(avr)
+print(avr)
 l.append(["Среднее значение", str(avr), ""])
 df = pd.DataFrame([l])
 df.columns = ['col1', 'col2', 'col3', 'col4']
---
+++ ex_time.py
@@ -0,0 +1,4 @@
+import time
+for i in range(10):
+    print(time.time_ns())
+    time.sleep(0.5)
