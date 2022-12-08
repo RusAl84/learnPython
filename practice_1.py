30d3d867ee6c1a67f7ae94c74e53a145559fd9ed
Aleksey <rusal@bk.ru>
2022-12-08 09:26:45 +0300

    add practice 1 statr

---
+++ practice_1.py
@@ -0,0 +1,21 @@
+﻿import datetime
+import random
+
+"""
+Datatype examples;
+Working with strings without control flow;
+"""
+
+def syntax_example():
+    a = 10
+    b = 5
+    if a > b:
+        print("A больше B")
+        print(a - b)
+    else:
+        print("B больше или равно A")
+        print(b - a)
+    print("Конец")
+
+if __name__ == "__main__":
+    syntax_example()
