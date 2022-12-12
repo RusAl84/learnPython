28c5eadf5613af9a34dc5e9a531a90e4746f27ea
Aleksey <rusal@bk.ru>
2022-12-12 12:10:54 +0300

    add pr 5

---
+++ DevOps/practice_5.py
@@ -0,0 +1,320 @@
+﻿import datetime
+import io
+import os
+import random
+import shutil
+import glob
+import hashlib
+from collections.abc import Callable
+import re
+import string
+import zlib
+import gzip
+import binascii
+import pickle
+
+
+COPY_PATH = "./DevOps/output/f1.txt"
+DEF_ENC = "utf-8"
+fpath = "./DevOps/input/f1.txt"
+
+
+def contextless_open(fpath=fpath):
+    fh = open(fpath, "r")
+    data = fh.read()
+    print(data[:100])
+    fh.close()
+    fh = open(fpath, "rt", encoding="utf-8")
+    data = fh.read()
+    print(data[:100])
+    fh.close
+
+
+def bytes2human(n: int, format="%(value).1f%(symbol)s") -> str:
+    """int N Bytes -> str Unix Style Size
+    bytes2human(100001221) -> '95.4M'
+    """
+    symbols = ("B", "K", "M", "G", "T", "P", "E", "Z", "Y")
+    prefix = {}
+    for i, s in enumerate(symbols[1:]):
+        prefix[s] = 1 << (i + 1) * 10
+    for symbol in reversed(symbols[1:]):
+        if n >= prefix[symbol]:
+            value = float(n) / prefix[symbol]
+            return format % locals()
+    return format % dict(symbol=symbols[0], value=n)
+
+
+def with_open(fpath):
+    with open(fpath, "rt", encoding="utf-8") as fh:
+        for idx, line in enumerate(fh.readlines()):
+            if idx > 2:
+                break
+            line_ending_less = line.strip("\n")
+            print(f"{idx+1}:\t{line_ending_less}")
+    print(
+        "Вне блока file handler уже отдан GC, и следить за его закрытием и удалением не надо."
+    )
+
+
+def os_path_methods(fpath=fpath):
+    print(
+        f"File directory: {os.path.dirname(fpath)}\tFilename: {os.path.basename(fpath)}"
+    )
+    print(f"Absolute path: {os.path.realpath(fpath)}")
+    print(f"Exists and file: {os.path.isfile(fpath)}")
+    print(f"Definetly not exist: './sfsdfas.txt' {os.path.isfile('./sfsdfas.txt')}")
+    print(f"Creation timestamp (secs from epoch): {os.path.getctime(fpath)}")
+    size_in_B = os.path.getsize(fpath)
+    print(f"Size in bytes: {size_in_B} B; -h size: {bytes2human(size_in_B)}")
+
+
+def simple_text_file_operations(fpath=fpath):
+    shutil.copyfile(fpath, COPY_PATH)
+    with open(COPY_PATH, "wt", encoding="utf-8") as fh:
+        fh.write("Wt флаг затирает содержимое файла.")
+        fh.write(
+            f"{os.linesep}write пишет в конец файла. Без перевода строки.{os.linesep}"
+        )
+        fh.writelines(
+            [
+                l + os.linesep
+                for l in [
+                    "Надо добавить разделитель строки",
+                    "Для каждого элемента списка",
+                ]
+            ]
+        )
+        print(
+            "Print'ом тоже можно писать в экземляры file, т.к. они resolve`нуться в io.TextWrapper",
+            file=fh,
+        )
+
+
+def list_dir(fpath="./Devops/"):
+    if not os.path.isdir(fpath):
+        raise ValueError(f"Expected valid directory path. Got {fpath}")
+    listed_files = []
+    print(f"Listing {os.path.realpath(fpath)}...")
+    # начните с простого листинга
+    # потом докручивайте по одному до ls -alt
+    # как права под виндой сделать я не знаю
+    for fname in os.listdir(fpath):
+        fullpath = os.path.realpath(fpath + os.sep + fname)
+        size = os.path.getsize(fullpath)
+        ctime = os.path.getctime(fullpath)
+        dt = datetime.datetime.fromtimestamp(ctime)
+        is_dir = "Dir" if os.path.isdir(fpath) else "File"
+        print(f"{fname:12}\t{dt.strftime('%x %X')}\t{size}\t{is_dir}")
+
+
+def globing(fpath=""):
+    for fp in glob.glob(fpath, recursive=True):
+        print(os.path.realpath(fp))
+
+
+def small_files_routine(
+    fpath="./devops/input/log.log",
+    fn_for_line=print,
+    enc="utf-8",
+    trim=True,
+    **kwargs,
+):
+    """
+    :param fpath: str pathlike Путь до файла.
+    :param fn_for_line: runnable  Ф-я применяемая к каждой строке, результат кладем в лист и возвращаем.
+    :param enc: str кодировка
+    :param trim: bool Срезать ли line break`и до передачу в ф-ю
+    :return: list of results
+    """
+    res = []
+    # io open, позволяет на ходу менять кодировки, и заменять/игнорировать ошибки,
+    # если символ не удается декодировать.
+    with io.open(fpath, encoding=enc, errors="replace") as fh:
+        # пока размер в B, K, M  можно смело делать readlines...
+        for line in fh.readlines():
+            if trim:
+                line = line.strip("\r\n")
+            res_fn = fn_for_line(line, **kwargs)
+            if res_fn:
+                res.append(res_fn)
+    return res
+
+
+def file_handler_methods(fpath=fpath, cp_path=COPY_PATH):
+    shutil.copyfile(fpath, cp_path)
+    fh = open(cp_path, encoding="utf-8")
+    print(f"Уставливает указатель на позицию: {fh.seek(0)}\n{fh.readline()}")
+    print(f"Уставливает указатель на позицию: {fh.seek(222)}\n{fh.readline()}")
+    print(f"Указатель переместился на {fh.tell()} позицию")
+    fh.readline()
+    print(f"Указатель переместился на {fh.tell()} позицию")
+    print(f"Файловый дескриптор,инт.Если улетел во много, вы где-то не закрыли контекст/файл: {fh.fileno()}")
+    print(f"{fh.seekable()}")
+
+######################################Archives#workflow#################################################################
+def compresson101():
+    data = 'Hello. I am Dmitri Karamazov and the world is my father'
+    lst = string.ascii_letters
+    hard_to_compress = "".join(random.choice(lst) for _ in range(256))
+
+    compressed_data = zlib.compress(data.encode(), 2)
+    print(f'Original data: {data}')
+    print(f'Compressed data: {binascii.hexlify(compressed_data)}')
+    compressed_data = zlib.compress(hard_to_compress.encode(), 2)
+    print(f'Random sequence: {hard_to_compress}')
+    print(f'Compressed data: {binascii.hexlify(compressed_data)}')
+
+def figure_out_compression_ratio(fpath=fpath, lvl=None):
+    if not lvl:
+        lvl = zlib.Z_BEST_COMPRESSION
+    original_data = open(fpath, 'rb').read()
+    compressed_data = zlib.compress(original_data, lvl)
+
+    compress_ratio = (float(len(original_data)) - float(len(compressed_data))) / float(len(original_data))
+    print('Compressed: %d%%' % (100.0 * compress_ratio))
+
+
+def extract_2_file(src_path=fpath, target_path="./output"):
+    if not os.path.isfile(src_path):
+        raise FileNotFoundError(f"Expected archive file at {src_path}")
+    if os.path.isdir(target_path):
+        src_name = os.path.basename(src_path)
+        if src_name.find('.gz')!=-1:
+            src_name=src_name[:src_name.find('.gz')]
+        target_path=target_path+os.sep+src_name
+    with gzip.open(src_path, 'rb') as f_in:
+        with open(target_path, 'wb') as f_out:
+            shutil.copyfileobj(f_in, f_out)
+
+
+#what's inside...
+def decompress_with_chunks(fpath, CHUNKSIZE = 1024):
+    data = zlib.decompressobj()
+    my_file = open(fpath, 'rb')
+    buf = my_file.read(CHUNKSIZE)
+    while buf:
+        decompressed_data = data.decompress(buf)
+        buf = my_file.read(CHUNKSIZE)
+
+    decompressed_data += data.flush()
+    return decompressed_data
+#######################################Common formats#################################################################
+class A:
+    def __init__(self, l):
+        self.l = l
+        self.dt = datetime.datetime.now()
+
+    def pop(self):
+        return self.l.pop()
+
+def pickle_exmaple():
+    a = A([x for x in range(121, 191)])
+    with open('./devops/output/a.pickle', 'wb') as fhw:
+        pickle.dump(a, fhw)
+    with open('./devops/output/a.pickle', 'rb') as fhr:
+        b = pickle.load(fhr)
+    for x in b.l:
+        print(x)
+
+def reverse_readline(path: str, buf_size=8192, split_char='\n', enc="utf-8"):
+    """A generator that returns the lines of a file in reverse order"""
+    ###Most of the time we work with insanly big files, seeking stuff from the end of the file
+    if not os.path.isfile(path):
+        raise ValueError(f"Expected file at {path}. Failed to fetch.")
+    with open(path, encoding=enc) as fh:
+        segment = None
+        offset = 0
+        fh.seek(0, os.SEEK_END)
+        file_size = remaining_size = fh.tell()
+        while remaining_size > 0:
+            offset = min(file_size, offset + buf_size)
+            fh.seek(file_size - offset)
+            buffer = fh.read(min(remaining_size, buf_size))
+            remaining_size -= buf_size
+            lines = buffer.split(split_char)
+            if segment is not None:
+                # If the previous chunk starts right from the beginning of line
+                # do not concat the segment to the last line of new chunk.
+                # Instead, yield the segment first
+                if buffer[-1] != split_char:
+                    lines[-1] += segment
+                else:
+                    yield segment
+            segment = lines[0]
+            for index in range(len(lines) - 1, 0, -1):
+                if lines[index]:
+                    yield lines[index]
+        if segment is not None:
+            yield segment
+
+########################################################################################################################
+def unix_style_file_operations(path_glob:str, regExp_str:str, fn: Callable):
+    res = []
+    pattern=re.compile(regExp_str)
+    for file in glob.glob(path_glob, recursive=True):
+        if os.path.isfile(file):
+            fname = os.path.realpath(file)
+            with io.open(file, encoding='utf-8', errors='replace') as log_f_handle:
+                try:
+                    for line in log_f_handle.readlines():
+                        for m in pattern.finditer(line):
+                            d = (m.groupdict())
+                            if len(d.keys()) == 0:
+                                continue
+                            else:
+                                print(f"Doing some buisness logic on {d}")
+                            res.append(d)
+                except UnicodeDecodeError as e:
+                    print(f"Error with decoding {fname}")
+                    print(str(e))
+                    with open("./error.log", "at") as f_h:
+                        f_h.writelines([fname, str(e), os.linesep])
+    return res
+
+
+
+if __name__ == "__main__":
+    # print("Files")
+    # contextless_open()
+    # os_path_methods(fpath=fpath)
+    # with_open(fpath)
+    # simple_text_file_operations()
+    # list_dir()
+    # globing("./devops/pr*")
+    # globing("./.venv/Scripts/*.exe")
+
+    # small_files_routine()
+    # С большими файлами так не прокатит:
+    # res = small_files_routine()
+    # print(res)
+    
+    # file_handler_methods()
+
+    # sizes_l = [1024, 67_108_864, 10_069_547_520]
+    # for size_in_B in sizes_l:
+    #     print(f"Converting {size_in_B}B to human readable: {bytes2human(size_in_B)}")
+
+    #In-to zlib
+    # compresson101()
+    # figure_out_compression_ratio()
+    # figure_out_compression_ratio(fpath="./devops/input/log.log", lvl=None)
+    # extract_2_file()
+    
+    # pickle_exmaple()
+    
+    #Работа с огромными файлами, чаще всего имеет смысл с конца. Заодно на генератор посмотрим:
+    #Если вперед читать, идея таже токо там еще chunk_size вперед едет, и seek в конец нет.
+    # Эквивалент tail -n 10
+    # gen = reverse_readline(fpath)
+    # for i in range(10):
+    #     line = next(gen)
+    #     print(f"{i}:\t{line}")
+    # # Добираем остаток
+    # for line in gen:
+    #     print(line)
+
+    # bonus1 unix style glob + re = super grep
+    # За регэкспы объяснять отдельно. И это одна из тех штук где бесполезно объяснять можно токо взять.
+    unix_style_file_operations('./devops/*',r"^[ ]*(?P<import>(import |from ))(?P<package>[\w. ]*)", print)
+    
---
+++ DevOps/practice_5_offtop.py
@@ -0,0 +1,97 @@
+﻿import os
+import glob
+import datetime
+
+class AcknowledgedFile:
+    def __init__(self, fpath, scheme=None, gen=None):
+        if not os.path.isfile(fpath):
+            raise ValueError(f"Trying to describe non existing file at {fpath}")
+        self.reg_dt = datetime.now()
+        self.rel_path = fpath
+        self.scheme = scheme
+        self.generator = gen
+        self.calc_md5()
+
+    def __repr__(self):
+        return f"File Details {self.rel_path}, Registred at: {self.reg_dt.strftime('%d.%m.%y %X')}\n md5: {self.md5}"
+
+    def set_mask(self, mask:str):
+        self.mask = mask
+
+    def calc_md5(self):
+        self.md5 = hashlib.md5(open(self.rel_path, 'rb').read()).hexdigest()
+
+    def calc_crc(self):
+        self.sha256 = hashlib.sha256(open(self.rel_path, 'rb').read()).hexdigest()
+
+    @staticmethod
+    def calc_md5_of(fpath):
+        if not os.path.isfile(fpath):
+            raise ExpectedFileException("Expected file at {fpath}")
+        return hashlib.md5(open(fpath, 'rb').read()).hexdigest()
+
+class Directory_watcher:
+    """Следитель за состояниям файлов на коленке.
+    Класс, котороый передается какому-то шеделеру (скрипт в кроне, либо если кровавый прод RabbitMQ.
+    Шедулер дергает ран по расписанию.)"""
+
+    def __init__(self, path_2_watch, config=None, action_to_perform=None):
+        """
+        :param path_2_watch: directory that should be wathced for changes
+        :param action_to_perform:  function to be called on all new files
+        """
+        if not os.path.isdir(abs_path):
+            raise ExpectedFileException(f"Expected dir at {abspath}. Aborting creation")
+
+        self.acknowledged_files_index = {} #only names:hashes, should be resorted from time to time.
+        self.acknowledged_files =[] #file details as instances of AcknowledgedFile
+        self.fn_2_call = action_to_perform
+        self.cfg = config
+        self.abs_path = os.path.realpath(path_2_watch)
+
+    def watch(self):
+        for file in glob.glob(self.abs_path+'**'):
+            if file in self.acknowledged_files_index.keys():
+                md5 = AcknowledgedFile.calc_md5_of(file)
+                if md5 == self.acknowledged_files_index.get(file):
+                    continue
+                else:
+                    self.update(file)
+            else:
+                self.add(file)
+
+    def add(self, file_p):
+        gen = None
+        if self.fn_2_call:
+            try:
+                gen = self.fn_2_call(file_p)
+            except CustomException as ex:
+                raise CustomException(f"Function {fn_2_call} failed on {file_p}.\n Original ex: {str(ex)}", ex)
+
+        akn_f = AcknowledgedFile(cfg=self.cfg, fpath=file_p)
+        self.acknowledged_files.append(akn_f)
+        self.acknowledged_files_index.update({file_p:akn_f.md5})
+
+    def remove(self, file_p):
+        self.acknowledged_files_index.pop(file_p)
+        for  idx, akn_f in enumerate(self.acknowledged_files):
+            if akn_f.rel_path == file_p:
+                removed = self.acknowledged_files.pop(idx)
+                return removed
+        print(f"{file_p} not found!")
+        return None
+
+    def update(self, file_p):
+        self.remove(file_p)
+        self.add(file_p)
+
+
+class CustomException(Exception):
+    pass
+
+class ExpectedFileException(CustomException):
+    pass
+
+class FnException(CustomException):
+    pass
+
