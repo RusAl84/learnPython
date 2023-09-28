689179de9a3ed18df774e6a6d1c01b9942b58166
Aleksey <rusal@bk.ru>
2023-09-28 13:36:56 +0300

    1

--- Ya/ex5_1/t4.py
+++ Ya/ex5_1/t4.py
@@ -4,22 +4,26 @@ class Programmer:
         self.status = _status
         self.w_time = 0
         self.salery = 0
-        self.smap ={"Junior": 10, "Middle": 15, "Senior": 20}
-
-        
+        self.smap = {"Junior": 10, "Middle": 15, "Senior": 20}
+        self.bonus = 0
 
     def work(self, _t):
+        self.salery += (self.smap[self.status] + self.bonus) * _t
         self.w_time += _t
 
     def info(self):
-        #<имя> <количество отработанных часов>ч. <накопленная зарплата>тгр.
-        return f"{self.name} {self.w_time}ч. {self.salery}тгр."
-    
+        # <имя> <количество отработанных часов>ч. <накопленная зарплата>тгр.
+        return f"{self.name} {self.w_time}ч. {self.salery}тгр. {self.status}"
+
     def rise(self):
-        cur_ind_status = list( self.smap.keys()).index(self.status)
+        cur_ind_status = list(self.smap.keys()).index(self.status)
         cur_ind_status += 1
-        print(list(self.smap.keys())[1])
-        self.status =  list(self.smap.keys())[int(cur_ind_status)]
+        # print(list(self.smap.keys())[1])
+        if cur_ind_status < 3:
+            self.status = list(self.smap.keys())[int(cur_ind_status)]
+        else:
+            self.bonus += 1
+
 
 if __name__ == '__main__':
     programmer = Programmer('Васильев Иван', 'Junior')
@@ -31,6 +35,6 @@ if __name__ == '__main__':
     programmer.rise()
     programmer.work(250)
     print(programmer.info())
-    # programmer.rise()
-    # programmer.work(250)
-    # print(programmer.info())
+    programmer.rise()
+    programmer.work(250)
+    print(programmer.info())
