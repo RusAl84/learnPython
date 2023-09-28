278ebfe2baeab13846408e95e7d9176631b12a7a
Aleksey <rusal@bk.ru>
2023-09-28 17:01:40 +0300

    1

--- Ya/ex5_1/t5.py
+++ Ya/ex5_1/t5.py
@@ -6,9 +6,17 @@ class Rectangle:
     def perimeter(self):
         x = abs(self.point1[0]-self.point2[0])
         y = abs(self.point1[1]-self.point2[1])
-        return round(2*x+2*y,2)
+        return round(2*x+2*y, 2)
+
+    def area(self):
+        x = abs(self.point1[0]-self.point2[0])
+        y = abs(self.point1[1]-self.point2[1])
+        return round(x*y, 2)
 
 
 if __name__ == '__main__':
-    rect = Rectangle((3.2, -4.3), (7.52, 3.14))
-    print(rect.perimeter())
+    # rect = Rectangle((3.2, -4.3), (7.52, 3.14))
+    # print(rect.perimeter())
+
+    rect = Rectangle((7.52, -4.3), (3.2, 3.14))
+    print(rect.area())
