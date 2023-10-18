1cb40799e02121a9049ed27e52dc230426650c49
Aleksey <rusal@bk.ru>
2023-10-18 11:48:42 +0300

    fix

--- Ya/ex5_1/t7.py
+++ Ya/ex5_1/t7.py
@@ -4,19 +4,42 @@ class Rectangle:
         self.point2 = point2
 
     def get_pos(self):
-        return (self.point1[0], self.point2[1])    
-    
+        if self.point1[0] <= self.point2[0]:
+            return (self.point1[0], self.point2[1])
+        else:
+            return (self.point2[0], self.point2[1])
+
     def get_size(self):
-        x = abs(self.point1[0]-self.point2[0])
-        y = abs(self.point1[1]-self.point2[1])
+        x = abs(self.point1[0] - self.point2[0])
+        y = abs(self.point1[1] - self.point2[1])
         return (round(x, 2), round(y, 2))
-    
+
     def move(self, _x, _y):
-        self.point1 = (round(self.point1[0] + _x,2), round(self.point1[1] + _y,2))
-        self.point2 = (round(self.point2[0] + _x,2), round(self.point2[1] + _y,2))
+        self.point1 = (round(self.point1[0] + _x, 2),
+                       round(self.point1[1] + _y, 2))
+        self.point2 = (round(self.point2[0] + _x, 2),
+                       round(self.point2[1] + _y, 2))
+
+    def resize(self, _x, _y):
+        if self.point1[0] > self.point2[0]:
+            self.point1 = (self.point2[0] , self.point2[1])
+            self.point2 = (self.point2[0] + _x, self.point2[1] + _y)
+        else:
+            self.point2 = (round(self.point2[0] + _x, 2),
+                           round(self.point2[1] + _y, 2))
+
 
 if __name__ == '__main__':
-    rect = Rectangle((3.2, -4.3), (7.52, 3.14))
-    print(rect.get_pos()   , rect.get_size())
-    rect.move(1.32, -5)
+    # rect = Rectangle((3.2, -4.3), (7.52, 3.14))
+    # print(rect.get_pos()   , rect.get_size())
+    # rect.move(1.32, -5)
+    # print(rect.get_pos(), rect.get_size())
+    # (3.2, 3.14) (4.32, 7.44)
+    # (4.52, -1.86) (4.32, 7.44)
+
+    rect = Rectangle((7.52, -4.3), (3.2, 3.14))
     print(rect.get_pos(), rect.get_size())
+    rect.resize(23.5, 11.3)
+    print(rect.get_pos(), rect.get_size())
+    # (3.2, 3.14) (4.32, 7.44)
+    # (3.2, 3.14) (23.5, 11.3)
