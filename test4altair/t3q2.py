811252c3ad1769eb6b7a46624c09382aa325c64a
Aleksey <rusal@bk.ru>
2023-10-18 14:03:29 +0300

    fix

--- test4altair/t3q2.py
+++ test4altair/t3q2.py
@@ -2,4 +2,4 @@ import pymorphy2
 
 morph = pymorphy2.MorphAnalyzer()
 word = morph.parse("Синхрофазатроны")[0]
-print(word.normal_form)
+print(word)
---
+++ test4altair/t3q4.py
@@ -0,0 +1,3 @@
+import datetime
+date = datetime.date(2023, 9, 3)
+print(date)
